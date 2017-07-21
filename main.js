8f5f56bee41fba5ce09a706c2944cd99c1bf9a47
gproduct <gproduct1379@yahoo.com>
2017-07-21 18:27:29 +0200

    Added new files, installed bootstrap and jquery

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
-const electron = require('electron')
-const {app, BrowserWindow} = electron
+const electron = require('electron');
+const {app, BrowserWindow} = electron;
 
 app.once('ready', () => {
-	let win = new BrowserWindow({width: 800, height:600})
-	win.loadURL('https://github.com')
-})
+	let win = new BrowserWindow({
+		width: 			1100, 
+		height: 		550,
+		resizable: 		false,
+		maximizable: 	false,
+		title: 			'Projector',
+		//backgroundColor:'#6C7A89'
+
+	});
+
+	win.toggleDevTools();
+	win.loadURL(`file://${__dirname}/src/html/index.html`);
+});
+
+app.on('browser-window-created', (e, window) => {
+	window.setMenu(null);
+});
+
