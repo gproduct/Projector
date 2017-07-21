601feff0b894dc999ddc93aa617baaaf214b355c
gproduct <gproduct1379@yahoo.com>
2017-07-21 21:24:04 +0200

    Added database, did some animations

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-const electron = require('electron');
-const {app, BrowserWindow} = electron;
+const 	electron 				= require('electron');		// electron package
+const 	{app, BrowserWindow} 	= electron;					// getting elements from electron
+var 	TinyDB 					= require('tinydb');		// database package
 
+db = new TinyDB('./data.db');								// accessing the database
+
+/*
+ *	Function: 	 app.once('ready')
+ * 	Description: Executes when the app is ready and sets parameters
+ */
 app.once('ready', () => {
 	let win = new BrowserWindow({
 		width: 			1100, 
@@ -12,11 +19,57 @@ app.once('ready', () => {
 
 	});
 
-	//win.toggleDevTools();
-	win.loadURL(`file://${__dirname}/src/html/index.html`);
+	//win.toggleDevTools();										// used for debuging, the dev tools show up
+	win.loadURL(`file://${__dirname}/src/html/index.html`);		// executes the index.html (first time setup)
 });
 
 app.on('browser-window-created', (e, window) => {
 	window.setMenu(null);
 });
 
+/*********************************************************************************/
+
+/*
+ *	Function: 	 db.onReady
+ * 	Description: Executes when the database is ready for operation
+ */
+db.onReady = function() {
+  console.log('database is ready for operating');
+}
+
+/*
+ *	Function: 	 update_db
+ * 	Description: Uses exports for electron to access it. Updates the database
+ */
+exports.update_db = (input_key, input_value) => {
+	db.setInfo(input_key, input_value, function(err, key, value) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    
+    console.log('[update_db] ' + key + ' : ' + value);
+  });
+}
+
+/*
+ *	Function: 	 update_db
+ * 	Description: Uses exports for electron to access it. Reads the database and returns 
+ */
+exports.read_db = (input_key) => {
+	var return_value;
+
+	db.getInfo(input_key, function(err, key, value) {
+	    if (err) {
+	      console.log(err);
+	      return;
+	    } else {
+	    	console.log('[read_db] ' + key + ' : ' + value);
+	    	return_value = value;
+	    }
+  	});
+
+	return return_value;
+}
+
+/*********************************************************************************/
--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,15 @@
-var jquery = require('jquery');
+const remote 	= require('electron').remote;
+const main		= remote.require('./main.js');
+var $ 			= require('jquery');
+
+/*main.update_db('swag','FULL');
+var returned = main.read_db('swag');
+console.log(returned);*/
+
+setTimeout(function() {}, 1500);
+
+$("#get_started").fadeIn(1800, () => {
+	$("#get_started_button").fadeIn(1300);
+});
 
 
