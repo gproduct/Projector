4197ec850cd2ff1c77680b0bd4d8a62d80a6c16a
gproduct <gproduct1379@yahoo.com>
2017-07-21 21:43:36 +0200

    Checking for number of projects

--- main.js
+++ main.js
@@ -4,12 +4,13 @@ var 	TinyDB 					= require('tinydb');		// database package
 
 db = new TinyDB('./data.db');								// accessing the database
 
+let win;													// first main window
 /*
  *	Function: 	 app.once('ready')
  * 	Description: Executes when the app is ready and sets parameters
  */
 app.once('ready', () => {
-	let win = new BrowserWindow({
+	win = new BrowserWindow({
 		width: 			1100, 
 		height: 		550,
 		resizable: 		false,
@@ -20,7 +21,8 @@ app.once('ready', () => {
 	});
 
 	//win.toggleDevTools();										// used for debuging, the dev tools show up
-	win.loadURL(`file://${__dirname}/src/html/index.html`);		// executes the index.html (first time setup)
+
+	
 });
 
 app.on('browser-window-created', (e, window) => {
@@ -34,7 +36,15 @@ app.on('browser-window-created', (e, window) => {
  * 	Description: Executes when the database is ready for operation
  */
 db.onReady = function() {
-  console.log('database is ready for operating');
+  	console.log('database is ready for operating');
+
+  	var number_of_projects = parseInt(read_db("num_of_projects"));	// reading how many projects are there
+	if(number_of_projects == 0) {
+		win.loadURL(`file://${__dirname}/src/html/index.html`);		// executes the index.html (first time setup)
+	} else {
+		
+
+	}
 }
 
 /*
@@ -57,6 +67,23 @@ exports.update_db = (input_key, input_value) => {
  * 	Description: Uses exports for electron to access it. Reads the database and returns 
  */
 exports.read_db = (input_key) => {
+	var return_value;
+
+	db.getInfo(input_key, function(err, key, value) {
+	    if (err) {
+	      console.log(err);
+	      return;
+	    } else {
+	    	console.log('[read_db] ' + key + ' : ' + value);
+	    	return_value = value;
+	    }
+  	});
+
+	return return_value;
+}
+
+// local function
+function read_db(input_key) {
 	var return_value;
 
 	db.getInfo(input_key, function(err, key, value) {
--- src/js/index.js
+++ src/js/index.js
@@ -2,9 +2,6 @@ const remote 	= require('electron').remote;
 const main		= remote.require('./main.js');
 var $ 			= require('jquery');
 
-/*main.update_db('swag','FULL');
-var returned = main.read_db('swag');
-console.log(returned);*/
 
 setTimeout(function() {}, 1500);
 
@@ -12,4 +9,6 @@ $("#get_started").fadeIn(1800, () => {
 	$("#get_started_button").fadeIn(1300);
 });
 
-
+$("#get_started_button").click(function() {
+	
+});
