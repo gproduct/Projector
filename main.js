877bc57fee6e0055ea939e9ea988507ca04f54f8
gproduct <gproduct1379@yahoo.com>
2017-07-24 22:05:41 +0200

    Added 'database' and export functions

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
-const 	electron 				= require('electron');		// electron package
-const 	{app, BrowserWindow} 	= electron;					// getting elements from electron
+const 	electron 				= require('electron');							// electron package
+const 	{app, BrowserWindow} 	= electron;										// getting elements from electron
 
-let win;													// first main window
+// https://www.npmjs.com/package/simple-json-db
+const JSONdb = require('simple-json-db');										// json database for making this whole project portable
+
+var path_for_data = "/home/silard_g/development/Projector/examples/test.json"; 	// will be changable by user
+const db = new JSONdb(path_for_data);
+
+let win;																		// first main window
+
 /*
  *	Function: 	 app.once('ready')
  * 	Description: Executes when the app is ready and sets parameters
@@ -17,11 +24,30 @@ app.once('ready', () => {
 
 	});
 
-	//win.toggleDevTools();										// used for debuging, the dev tools show up
-
-	
+	win.toggleDevTools();										// used for debuging, the dev tools show up
+	win.loadURL(`file://${__dirname}/src/html/index.html`);
 });
 
 app.on('browser-window-created', (e, window) => {
 	window.setMenu(null);
 });
+
+/***************************************************************************/
+
+/*
+ *	Function: 	 exports.update_db = (key, value)
+ * 	Description: Function that lets other files update the database
+ */
+exports.update_db = (key, value) => {
+	db.set(key, value);
+	if(!(db.has(key)))
+		console.log("Database: ERROR, key set but really not");
+}
+
+/*
+ *	Function: 	 exports.update_db = (key, value)
+ * 	Description: Function that lets other files read the database
+ */
+exports.read_db = (key) => {
+	return db.get(key);
+}
